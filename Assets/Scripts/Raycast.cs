57262e8dbaafe68e73d30665544c2528d8b4bd96
NineNero <salmaurbano01@gmail.com>
2024-01-18 09:59:45 +0100

    Click

---
+++ Assets/Scripts/Raycast.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycast : MonoBehaviour
+{
+    // Apply a force to a rigidbody in the Scene at the point
+    // where it is clicked.
+
+    // The force with which the target is "poked" when hit.
+    float pokeForce;
+
+    public void Start()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log(Input.mousePosition);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.rigidbody != null)
+                {
+                    hit.rigidbody.AddForceAtPosition(ray.direction * pokeForce, hit.point);
+                }
+            }
+        }
+    }
+}
